bab21799dba4e7a52b09d001a64d2b1d20a4ebba
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-20 11:40:56 +0200

    done footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+images:any[]=[]
 
+ngOnInit(): void {
+    this.images=[
+      {title:'Facebook',
+        link:'https://www.facebook.com/',
+        image:'../../../assets/footer/fb.png'
+      },
+      {title:'Instagram',
+        link:'https://www.instagram.com',
+        image:'../../../assets/footer/ig.png'
+      },
+      {title:'Linkedin',
+        link:'https://www.linkedin.com',
+        image:'../../../assets/footer/lk.png'
+      }
+     ]
+}
 }
