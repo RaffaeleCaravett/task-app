384d41b46f6ae1449b2752105777f8a681b530bb
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-21 09:02:49 +0200

    doing skeleton loader

---
+++ src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-loader',
+  template: `
+  <div>
+
+  </div>`,
+  styleUrls: ['./skeleton-loader.component.scss']
+})
+export class SkeletonLoaderComponent implements OnInit{
+constructor(){}
+
+ngOnInit(): void {
+
+}
+}
