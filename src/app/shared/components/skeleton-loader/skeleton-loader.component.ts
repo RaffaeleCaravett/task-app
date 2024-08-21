a896648dbdd2215b8562bd6c520f179a652f57d3
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-21 09:09:23 +0200

    doing skeleton loader scss

--- src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
+++ src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton-loader',
   template: `
-  <div>
+  <div class="skelt-load loader">
 
   </div>`,
   styleUrls: ['./skeleton-loader.component.scss']
 })
 export class SkeletonLoaderComponent implements OnInit{
+  @Input() Cwidth:string='';
+  @Input() Cheight:string='';
+  @Input() circle:boolean=false;
+
 constructor(){}
 
 ngOnInit(): void {
 
 }
+
+getMyStyles(){
+  const myStyles = {
+    'width.px': this.Cwidth? this.Cwidth:'',
+    'height.px': this.Cheight? this.Cheight:'',
+    'border-radius': this.circle? '50%':''
+  }
+  return myStyles;
+}
+
 }
