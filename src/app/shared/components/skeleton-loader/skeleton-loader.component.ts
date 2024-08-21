fb135e2f7ae7f62489010c7101ef7dc3c18d7eda
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-21 09:35:26 +0200

    done skeleton loader

--- src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
+++ src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skeleton-loader',
   template: `
-  <div [ngStyle]="getMyStyles()" class="skelt-load loader">
+  <div [ngClass]="getMyClasses()" [ngStyle]="getMyStyles()" class="skelt-load loader">
 
   </div>`,
   styleUrls: ['./skeleton-loader.component.scss']
@@ -19,13 +19,17 @@ ngOnInit(): void {
 
 }
 
+getMyClasses(){
+  const myClasses = [
+    this.Cwidth||'',
+    this.circle? 'rounded-circle':''
+  ]
+  return myClasses[0] + ' ' + myClasses[2];
+}
 getMyStyles(){
   const myStyles = {
-    'width.px': this.Cwidth? this.Cwidth:'',
-    'height.px': this.Cheight? this.Cheight:'',
-    'border-radius': this.circle? '50%':''
+    'height.px': this.Cheight?this.Cheight:''
   }
   return myStyles;
 }
-
 }
